254240772905da62f1bd71eeccb302b8854b9767
ninnin92 <ninnin92.28@gmail.com>
2015-11-25 19:08:16 +0900

    15/11/25 no.3

--- tw_ccf_R.py
+++ tw_ccf_R.py
@@ -13,9 +13,9 @@ import math
 ################################################
 #  宣言
 ################################################
-option = "A"
-maxlag = 3  # 最大のラグ数（正負にこの数だけズラす）
-window = 20  # 何個の要素を持った窓にするか
+option = "C"
+maxlag = 1  # 最大のラグ数（正負にこの数だけズラす）
+window = 10  # 何個の要素を持った窓にするか
 by     = 1      # 窓から窓へは何個ずつ増えるか（いくつ被るのを許容するか）
 n_overlap = window - by
 
@@ -24,68 +24,83 @@ n_overlap = window - by
 ################################################
 
 
-def buffer(s, lag):
-    s_list = []
+def tw_ccf(d1, d2, maxlag, window, by):
 
-    for i in range(lag + 1):
-        s_shift = s.shift(i).fillna(0)  # ずらしたデータの作成
-        s_shift.name = i
-        s_list.append(s_shift)
+    n = 0
+    x = d1
+    y = d2
 
-    df = pd.concat(s_list, axis=1)
+    nx = len(x)
+    ny = len(y)
 
-    return df
+    lag = list(np.arange(-maxlag, maxlag + 1))    # ラグ
+    #print(lag)
+    i_list = list(range(maxlag + 1, nx - maxlag - window + 1))
+    #print(i_list)
+    C = pd.DataFrame(0, index=np.arange(maxlag * 2 + 1),
+                                 columns=np.arange(len(i_list)))
 
+    for l in lag:
+        #print(l)
+        cor_list = []
 
-def window_df(s, window, by):
-    win_list = []
-    times = math.floor(((len(s) - window) / by) + 1)  # 窓の個数：floorで切り捨て
-    for n in range(0, times):
-        win_s = s[(0 + by * n) : (window + by * n)]  # データを窓に分割
-        win_s = win_s.reset_index(drop=True)
-        win_list.append(win_s)
+        if l <= 0:
+            for i in i_list:
+                X = x[i:i + window]
+                Y = y[i + l:i + window + l]
+                #print(X)
+                #print(Y)
 
-    df = pd.concat(win_list, axis=1)
+                Xm = X.mean(axis=0)
+                Ym = Y.mean(axis=0)
+                #print(Xm)
+                #print(Ym)
 
-    return df
+                Xsd = X.std(ddof=0)
+                Ysd = Y.std(ddof=0)
+                #print(Xsd)
+                #print(Ysd)
 
+                Z = [(Xi - Xm) * (Yi - Ym) / (Xsd * Ysd) for Xi, Yi in zip(list(X), list(Y))]
+                #print(sum(Z))
+                r = (1 / window) * sum(Z)
+                #print(r)
+                cor_list.append(r)
 
-# 標準化、偏差和を（母集団）標準偏差で割る
-# とりあえずは使わず
-def normalize(df):
-    df_set = pd.DataFrame()
-    df_list = []
+        if l > 0:
+            for i in i_list:
+                X = x[i - l:i + window - l]
+                Y = y[i :i + window]
+                #print(X)
+                #print(Y)
 
-    for i in range(0, len(df.columns)):
-        col = df.iloc[:, i]
+                Xm = X.mean(axis=0)
+                Ym = Y.mean(axis=0)
+                #print(Xm)
+                #print(Ym)
 
-        m = col.mean()
-        sd = col.std()
-        nz = col.sub(m).div(sd)
+                Xsd = X.std(ddof=0)
+                Ysd = Y.std(ddof=0)
+                #print(Xsd)
+                #print(Ysd)
 
-        df_list.append(nz)
+                Z = [((Xi - Xm) * (Yi - Ym)) / (Xsd * Ysd) for Xi, Yi in zip(list(X), list(Y))]
+                #print(sum(Z))
+                r = (1 / window) * sum(Z)
+                #print(r)
+                cor_list.append(r)
 
-    df_set = pd.concat(df_list, axis=1)
-    return df_set
-
-
-def win_ccf(d1, d2):
-
-    cor_list = []
-
-    # ある時点（列）での窓同士を選択して、相関解析 → それを各列
-    for x in range(0, len(d1.columns)):
-        a1 = np.array(d1.iloc[:, x])
-        a2 = np.array(d2.iloc[:, x])
-        cor = np.corrcoef(a1, a2)[0][1]
-        cor_list.append(cor)
+        #print(C)
+        #print(cor_list)
+        C.iloc[n, :] = cor_list
+        n += 1
 
     if len(cor_list) < 2:
         # print(len(cor_list))
         cor_list = cor_list[0]
         # print("hoge")
 
-    return cor_list
+    return(C)
 
 
 def heatmap_show(df, name, ex, trial, path, first):
@@ -96,11 +111,11 @@ def heatmap_show(df, name, ex, trial, path, first):
                           cbar_kws={"shrink": 0.6}, ax=ax)
 
     if first == "L":
-        focus = "    L - R : 0    R - L : 1"
+        focus = "    L - R : 0    R - L : -1"
     else:
-        focus = "    R - L : 0    L - R : -1"
+        focus = "    R - L : 0    L - R : 1"
 
-    title = "Windowed Xcorr  _  " + name + "-" + exp + "-" + trial + "\n" + "first:  " + first + focus
+    title = "Windowed Xcorr  _  " + name + "-" + ex + "-" + trial + "\n" + "first:  " + first + focus
     plt.title(title , fontsize=24, y=1.08)
     plt.xlabel("Time", fontsize=20, labelpad=20)  # Xのラベルサイズ
     plt.ylabel("Lag", fontsize=20, labelpad=20)  # Yのラベルサイズ
@@ -168,7 +183,7 @@ if __name__ == '__main__':
                 # 前処理
                 # 今はエラーを除外して分析、エラーを解析するなら複製データがいるかも
                 # 余分なものを削除、エラー関係のも
-                data_tr = data_tr[data_tr["step"] > 2]
+                data_tr = data_tr[data_tr["step"] > 0]
                 data_tr = data_tr[data_tr["error"] != 1]
 
                 data_L = data_tr[data_tr["L_Key"] == "L"].sort_values(by="step").reset_index()
@@ -194,63 +209,27 @@ if __name__ == '__main__':
                 nx = len(x)
                 ny = len(y)
 
-                # lag分ずらしたDfの準備
-                X = buffer(x, maxlag).sort_index(axis=1, ascending=False)  # +1カウントに合わせてできるように列を逆順にする
-                Y = buffer(y, maxlag)
-
-                #print(X)
-                #print(Y)
-
                 # 出力データフレームを仮作成
-                C = pd.DataFrame(0, index=np.arange(maxlag * 2 + 1),
-                                 columns=np.arange((nx - n_overlap) / (window - n_overlap)))
 
-                #print(C)
+                result = tw_ccf(x, y, maxlag, window, by)
 
-                # 相互相関解析
-                # -maxlag ~ 0まで：Y列を固定、X列を動かす
-                Yi = Y.iloc[:, 0]  # Yのフルサイズ列
-                Yi = window_df(Yi, window, by)  # 窓ごとに分割
-                #print(Yi)
-
-                for i in range(0, maxlag):
-                    Xi = X.iloc[:, i]  # ラグに合わせた列を選択
-                    Xi = window_df(Xi, window, by)  # 窓ごとに分割
-                    #print(Xi)
-                    xcor = win_ccf(Xi, Yi)  # すべての窓について相互相関
-                    C.iloc[i, :] = xcor  # 出力データフレームの書き換え
-                    #print(C)
-
-                # 0からmaxlagまで：X列を固定、Y列を動かす
-                Xi = X.iloc[:, maxlag]  # Yのフルサイズ列
-                Xi = window_df(Xi, window, by)
-                #print(Xi)
-
-                for i in range(0, maxlag + 1):
-                    Yi = Y.iloc[:, i]
-                    Yi = window_df(Yi, window, by)
-                    #print(Yi)
-                    xcor = win_ccf(Xi, Yi)
-                    #print(xcor)
-                    C.iloc[i + maxlag, :] = xcor
-
-                t = list(np.arange(1, nx, round((nx / len(Xi.columns)), 2)))  # Timeの表現（何個の窓ができるのか）
+                t = list(np.arange(1, nx, round((nx / len(result.columns)), 2)))  # Timeの表現（何個の窓ができるのか）
                 l = list(np.arange(-maxlag, maxlag + 1))           # ラグ
 
                 # 結果を表として出力
-                result = C
                 result.columns = [str(n) for n in t]  # タイムを文字列としての列名に
                 result.index = l                              # ラグをインデックスに
-                #print(result)
+                result.fillna(0)
+                print(result)
 
                 # Seabornでヒートマップを作図
                 # 一旦、縦長のデータフレームに変換する
-                df_melt = C
+                df_melt = result
                 new_col = t  # タイムは数字にしないと意図した通りの順番にならない
                 df_melt.columns = new_col
                 df_melt["Lag"] = l
                 df_melt = pd.melt(df_melt, id_vars=["Lag"], value_vars=new_col)  # ラグを基準に縦長にデータを変換
-                df_melt = df_melt.assign(ID=ID, exp=exp, trial_num=tr, age=age, sex=sex, first_trun=first_turn)
+                df_melt = df_melt.assign(ID=ID, exp=exp, trial_num=tr, age=age, sex=sex, first_turn=first_turn)
                 #print(df_melt)
                 df_sum = pd.concat([df_sum, df_melt])
                 df_melt.to_csv("csv_" + outpath + "/df_ccf_" + ID + "-" + exp + "-" + tr + ".csv", index=False)
@@ -263,5 +242,5 @@ if __name__ == '__main__':
                 heatmap_show(df_plot, ID, exp, tr, outpath, first_turn)
                 plt.close()
 
-    df_sum.to_csv("cross-corr-" + outpath + "2.csv", index=False)
+    df_sum.to_csv("cross-corr-" + outpath + ".csv", index=False)
     print("End Process")
---
+++ tw_ccf_R_sample.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding:utf-8
+# env: Python3.3
+
+import sys
+import numpy as np
+import pandas as pd
+import math
+
+
+################################################
+#  宣言
+################################################
+option = "C"
+maxlag = 3  # 最大のラグ数（正負にこの数だけズラす）
+window = 6  # 何個の要素を持った窓にするか
+by     = 1      # 窓から窓へは何個ずつ増えるか（いくつ被るのを許容するか）
+n_overlap = window - by
+
+################################################
+#  関数
+################################################
+
+
+def tw_ccf(d1, d2, maxlag, window, by):
+
+    n = 0
+    x = d1
+    y = d2
+
+    nx = len(x)
+    ny = len(y)
+
+    lag = list(np.arange(-maxlag, maxlag + 1))    # ラグ
+    #print(lag)
+    C = pd.DataFrame(0, index=np.arange(maxlag * 2 + 1), columns=np.arange(1))
+    print(C)
+
+    Xm = x.mean(axis=0)
+    Ym = y.mean(axis=0)
+
+    Xsd = x.std(ddof=0)
+    Ysd = y.std(ddof=0)
+
+    for l in lag:
+        #print(l)
+        c = 0
+
+        if l < 0:
+            for n in range((- l), nx):
+                print(n)
+                c = c + (x.iloc[n] - Xm) * (y.iloc[n + l] - Ym)
+
+            c = c / nx
+            r = c / (Xsd * Ysd)
+
+        if l >= 0:
+            for n in range(0, nx - l):
+                print(n)
+                c = c + (x.iloc[n] - Xm) * (y.iloc[n + l] - Ym)
+
+            c = c / nx
+            r = c / (Xsd * Ysd)
+
+        C.iloc[n, :] = r
+        n += 1
+
+    #if len(cor_list) < 2:
+        # print(len(cor_list))
+        #cor_list = cor_list[0]
+        # print("hoge")
+
+    return(C)
+
+
+################################################
+#  メイン処理
+################################################
+
+if __name__ == '__main__':
+
+    print("Run Process")
+    print("------------------")
+    print("Option:  " + option)
+    print("Maxlag:  " + str(maxlag))
+    print("Window:  "  + str(window))
+    print("By:  " + str(by))
+    print("------------------")
+
+    # 前処理
+    # 今はエラーを除外して分析、エラーを解析するなら複製データがいるかも
+    # 余分なものを削除、エラー関係のも
+
+    data_x = pd.DataFrame({"L": [1, 3, 1, 3, 1, 3], "R": [2.5, 1, 3, 1, 3, 1]})
+
+    print(data_x)
+
+
+    # 対象になる２列を指定、列の長さ確認
+    x = data_x.iloc[:, 0]  # 列数で指定したいときはiloc、ixは挙動がちんぷんかんぷん
+    y = data_x.iloc[:, 1]
+
+    #print(x)
+    #print(y)
+
+    nx = len(x)
+    ny = len(y)
+
+    # 出力データフレームを仮作成
+
+    result = tw_ccf(x, y, maxlag, window, by)
+
+    t = list(np.arange(1, nx, round((nx / len(result.columns)), 2)))  # Timeの表現（何個の窓ができるのか）
+    l = list(np.arange(-maxlag, maxlag + 1))           # ラグ
+
+    # 結果を表として出力
+    result.columns = [str(n) for n in t]  # タイムを文字列としての列名に
+    result.index = l                              # ラグをインデックスに
+    result.fillna(0)
+    print(result)
+
+    print("End Process")
